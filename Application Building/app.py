e997cd06c8fa57a9b181c5cc8fc9df146667f7dd
Hari42prathap <110247347+Hari42prathap@users.noreply.github.com>
2022-11-03 22:38:35 +0530

    Application Building

---
+++ Application Building/app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template, url_for
+
+app=Flask(__name__)
+
+@app.route('/')
+@app.route('/web detection.html')
+def Home():
+           return render_template("web detection.html")
+    
+@app.route('/')
+@app.route('/About.html')
+
+def About():
+       return render_template("About.html")
+
+
+@app.route('/Getstarted.html')
+def Getstarted():
+       return render_template("Getstarted.html")
+
+       
+       
+if __name__ == "__main__":   
+ app.run(debug=True)
+    
